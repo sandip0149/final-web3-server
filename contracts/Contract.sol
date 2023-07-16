47f60da3fbb58cef28166addfd3f13ced38757c9
sandip parida <paridasandip149@gmail.com>
2023-07-16 17:35:37 +0530

    server upload

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract SocialMedia{
+    struct post{
+        address owner;
+        string title;
+        string description;
+        uint likes;
+        uint createdat;
+
+    }
+
+    // post[] public  Allpost;
+    mapping (uint => post) Allpost;
+    uint public postNo;
+    function createPost(string memory _title,string memory _desc) public {
+         Allpost[postNo].owner = msg.sender;
+         Allpost[postNo].title = _title;
+         Allpost[postNo].description = _desc;
+         Allpost[postNo].createdat = block.timestamp;
+         postNo++;
+
+
+    }
+
+    function getPost() public  view returns(post[] memory){
+         post[] memory allp = new post[](postNo);
+
+        for(uint i=0;i<postNo ;i++)
+        {
+            post storage item = Allpost[i];
+            allp[i] = item;
+        }
+
+        return allp;
+    }
 }
